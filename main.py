a1d918846aabcc1e96bd07ef7fee224792f15071
KS <ss_kk_sk987@yahoo.co.jp>
2022-11-10 12:18:51 +0900

    グラフの見た目を途中まで整えた

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def main():
     fig = Figure()
     ax = fig.add_subplot(1, 1, 1) # 縦に1つ，横に1つ で分けた，右上
     ax.plot(x, y)
+    ax.set_title('$y = \\sin (\\pi x)$')
+    ax.set_xlabel('$x$')
+    ax.set_ylabel('$y$')
+    ax.axhline(color = '#777777') #水平線
+    ax.axvline(color = '#777777') #垂直線
     fig.savefig('out.png')
 
 if __name__ == '__main__':
