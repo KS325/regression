2b4567812a0cd000c6104f74efe1f9809b632e2b
KS <ss_kk_sk987@yahoo.co.jp>
2022-11-24 09:38:24 +0900

    学習サンプルをランダムに生成した

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ def main():
     x = np.linspace(-1, 1, 101) # -1~1の間を101等分（両端を含む）
     y = np.sin(np.pi * x)
 
+    #サンプルの準備
+    x_sample = np.random.uniform(-1, 1, (20, )) # 右端，左端，量
+    range_y = np.max(y) - np.min(y)
+    noise_sample = np.random.normal(0, range_y * 0.05, (20, ))
+    y_sample = np.sin(np.pi * x_sample) + noise_sample
+
     # グラフの作成
     fig = Figure()
     ax = fig.add_subplot(1, 1, 1) # 縦に1つ，横に1つ で分けた，右上
     ax.plot(x, y)
+    ax.scatter(x_sample, y_sample, color = 'red')
     ax.set_title('$y = \\sin (\\pi x)$')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
