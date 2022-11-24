fdf30b4d4a10d7d8c4cd1f78a523f8412b93bd45
KS <ss_kk_sk987@yahoo.co.jp>
2022-11-24 09:46:20 +0900

    グラフの凡例を日本語で表示した

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 from matplotlib.figure import Figure
+import japanize_matplotlib as _
+
 
 def main():
     # x, f(x)の準備
@@ -15,8 +17,9 @@ def main():
     # グラフの作成
     fig = Figure()
     ax = fig.add_subplot(1, 1, 1) # 縦に1つ，横に1つ で分けた，右上
-    ax.plot(x, y)
-    ax.scatter(x_sample, y_sample, color = 'red')
+    ax.plot(x, y, label = '真の関数 $f$')
+    ax.scatter(x_sample, y_sample, color = 'red', label = '学習サンプル')
+    ax.legend()
     ax.set_title('$y = \\sin (\\pi x)$')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
