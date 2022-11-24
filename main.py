92d43eedc48d8b62b176cf4296e14292a7a2af86
KS <ss_kk_sk987@yahoo.co.jp>
2022-11-24 10:28:01 +0900

    学習サンプルのx_i^pからなる行列X_sを計算した

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ def main():
     x = np.linspace(-1, 1, 101) # -1~1の間を101等分（両端を含む）
     y = np.sin(np.pi * x)
 
-    #サンプルの準備
+    # サンプルの準備
     x_sample = np.random.uniform(-1, 1, (20, )) # 右端，左端，量
     range_y = np.max(y) - np.min(y)
     noise_sample = np.random.normal(0, range_y * 0.05, (20, ))
     y_sample = np.sin(np.pi * x_sample) + noise_sample
+
+    # 多項式フィッティング
+    ## Xを作る
+    d = 3
+    p = np.arange(d + 1)[np.newaxis, :]
+    x_s = x_sample[:, np.newaxis]
+    X_s = x_s ** p
+    print(p)
+    print(x_s)
+    print(X_s)
 
     # グラフの作成
     fig = Figure()
@@ -23,8 +33,8 @@ def main():
     ax.set_title('$y = \\sin (\\pi x)$')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
-    ax.axhline(color = '#777777') #水平線
-    ax.axvline(color = '#777777') #垂直線
+    ax.axhline(color = '#777777') # 水平線
+    ax.axvline(color = '#777777') # 垂直線
     fig.savefig('out.png')
 
 if __name__ == '__main__':
